49acef790dcb3a8f3d7e5d4f081f0b7c51abbfe3
Autumn-eul <dlrkdmf1052@naver.com>
2024-11-16 00:23:29 +0900

    💡 메일 전송 app.py 구현
    메일 전송, 전송 완료 form 기능 구현, 메일을 보내기 위한 함수 작성

--- apps/sendmail/app.py
+++ apps/sendmail/app.py
@@ -1,7 +1,18 @@
 from flask import Flask, url_for, make_response, render_template, request, redirect
-import urllib.parse
+import os
+
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
 
 # 메일 전송 폼이 보이는 기능 구현
 @app.route("/contact")
@@ -9,12 +20,31 @@ def contact():
     return render_template("contact.html")
 
 @app.route("/contact/complete", methods = ['GET', 'POST'])
-def contect_complete():
+def contact_complete():
     if request.method == 'POST':
-        
-        return redirect('contact_complete')
+        username = request.form["name"] # form 에서 이름 정보 가져오기
+        email = request.form["email"]
+        message = request.form["message"]
+
+        send_email(
+            email,
+            "문의해주셔서 감사합니다.",
+            "contact_mail",
+            name = username,
+            message = message
+        )
+    
+        return redirect(url_for('contact_complete'))
     
     return render_template('contact_complete.html')
+
+# 메일을 보내기 위한 함수 작성
+def send_email(to, subject, template, **kwargs):
+    msg = Message(subject, recipients = [to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    msg.charset = 'utf-8'
+    mail.send(msg)
 
 if __name__ == "__main__":
     app.run(debug=True)

aa5f35604c993dc9a7aff68ad611074cf7b310dd
Autumn-eul <dlrkdmf1052@naver.com>
2024-11-15 11:11:44 +0900

    ✨ Route contact, contact_complete 구조 구현
    contact 는 메일 전송 폼, contact_complete 메일 보내기와redirect 기능

--- apps/sendmail/app.py
+++ apps/sendmail/app.py
@@ -1,8 +1,20 @@
-from flask import Flask, url_for, make_response, render_template, request # request 는 GET 인지 POST 인지 DELETE 인지 등등 구별해준다.
+from flask import Flask, url_for, make_response, render_template, request, redirect
 import urllib.parse
 
 app = Flask(__name__)
 
+# 메일 전송 폼이 보이는 기능 구현
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods = ['GET', 'POST'])
+def contect_complete():
+    if request.method == 'POST':
+        
+        return redirect('contact_complete')
+    
+    return render_template('contact_complete.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
